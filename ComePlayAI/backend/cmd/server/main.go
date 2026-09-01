c88c8011b31f9871be4e2256ab39da5635730431
sukrltluprichaset-dotcom <sukrlt.luprichaset@gmail.com>
2026-09-01 22:36:23 +0700

    เพิ่ม db_host ใน debug endpoint เพื่อเทียบเคส

--- ComePlayAI/backend/cmd/server/main.go
+++ ComePlayAI/backend/cmd/server/main.go
@@ -30,6 +30,7 @@ func main() {
 	mux.HandleFunc("/debug-env", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]interface{}{
+			"db_host":             cfg.DBHost,
 			"supabase_url":        cfg.SupabaseURL,
 			"supabase_url_length": len(cfg.SupabaseURL),
 			"service_key_length":  len(cfg.SupabaseServiceKey),
